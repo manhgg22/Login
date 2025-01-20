2a7c8e599ea6426d3528e20fcfdca6ca2efa7268
ducmanh2907 <manhldhe180916@fpt.edu.vn>
2025-01-20 14:38:30 +0700

    update

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {  Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  loginObj:any ={
+    "EmailId": "",
+    "Password": ""
+  };
+  
+  http = inject(HttpClient);
+  constructor (private router: Router){
+
+  }
+
+
+  onLogin(){
+    debugger;
+     this.http.post("https://freeapi.miniprojectideas.com/api/User/Login", this.loginObj).subscribe((res:any)=>{
+      debugger;
+      if(res.result){
+        alert("Login Success")
+        localStorage.setItem("Login", this.loginObj.User);
+        this.router.navigateByUrl('dashboard');
+      } else{
+        alert("Check username password")
+      }
+     })
+  }
+
+
+}
